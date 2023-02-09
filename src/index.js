dd72a199099719476978b71adabc0161234d5adc
elitmor <elitmor@gmail.com>
2023-02-09 11:37:57 +0200

    Add scroll to anchor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './index.html';
 import './index.scss';
-import './modules/theme-switcher';
 import './modules/burger-menu';
+import './modules/theme-switcher';
+import './modules/scroll-anchor';
---
+++ src/modules/scroll-anchor.js
@@ -0,0 +1,13 @@
+const scrollToAnchor = () => {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
+};
+
+scrollToAnchor();
