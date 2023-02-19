fd701af7399739560eeb497ee500c8135a80c4d1
elitmor <elitmor@gmail.com>
2023-02-19 11:30:28 +0200

    Resized typography

--- src/modules/burger-menu.js
+++ src/modules/burger-menu.js
@@ -1,5 +1,6 @@
 const menuButton = document.querySelector('.menu__button');
 const menuList = document.querySelector('.menu__list');
+const navLink = document.querySelectorAll('.menu__link');
 
 menuButton.addEventListener('click', () => {
   const expanded = menuButton.getAttribute('aria-expanded') === 'true';
@@ -7,3 +8,9 @@ menuButton.addEventListener('click', () => {
   menuButton.classList.toggle('menu__button--open');
   menuList.classList.toggle('menu__list--open');
 });
+
+const linkAction = () => {
+  menuList.classList.remove('menu__list--open');
+  menuButton.classList.toggle('menu__button--open');
+};
+navLink.forEach((n) => n.addEventListener('click', linkAction));
